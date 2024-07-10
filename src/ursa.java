523d8caf064a7f09d7ded22423afa667f53ff65b
Verat-hub <venkey482thalka@gmail.com>
2024-07-10 19:36:30 +0530

    fcuk you

---
+++ src/ursa.java
@@ -0,0 +1,9 @@
+public class ursa {
+    public static void main(String[] args) {
+        int kiss = 69;
+        int suck = 99;
+        String boobs = "I love tits";
+        System.out.println(kiss + suck + boobs);
+        System.out.println(kiss>suck);
+    }
+}
