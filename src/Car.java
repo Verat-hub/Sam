e52fff6ac5c7292d95d71472be619578afd72750
Verat-hub <venkey482thalka@gmail.com>
2024-07-12 01:43:28 +0530

    Hell_here

--- src/Car.java
+++ src/Car.java
@@ -2,4 +2,7 @@ public class Car {
     public void Horn() {
         System.out.println("Horn"); 
     }
+    public void Run() {
+        System.out.println("Run");
+    }
 }
--- src/ursa.java
+++ src/ursa.java
@@ -3,7 +3,9 @@ public class ursa {
     public static void main(String[] args) {
         Car car1 = new Car();
         car1.Horn();
+        car1.Run();
         Car car2 = new Car();
         car2.Horn();
+        car2.Run();
     }
 }
